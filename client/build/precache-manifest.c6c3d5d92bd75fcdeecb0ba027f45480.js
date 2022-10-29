d1349f8ebaa1554f429d874965cd17650389cc72
AditiDey <aditidey7080@gmail.com>
2022-10-29 15:59:52 +0530

    add

---
+++ client/build/precache-manifest.c6c3d5d92bd75fcdeecb0ba027f45480.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413cacea79a5afd1dabbf8bf66d48149",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43cf408609d47027ccf6",
+    "url": "/static/css/main.48042d3a.chunk.css"
+  },
+  {
+    "revision": "ebb582c9b0286ed45276",
+    "url": "/static/js/2.c5be0b1a.chunk.js"
+  },
+  {
+    "revision": "4d1f90a4d23fbef2d2b91d00380f8ac5",
+    "url": "/static/js/2.c5be0b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43cf408609d47027ccf6",
+    "url": "/static/js/main.132983a1.chunk.js"
+  },
+  {
+    "revision": "6b71224ab26933836c3b",
+    "url": "/static/js/runtime-main.9009ef15.js"
+  },
+  {
+    "revision": "4b5fffb08ce840d959f07789d27682d0",
+    "url": "/static/media/egg.4b5fffb0.svg"
+  },
+  {
+    "revision": "18dadafdd646309f101f5307740c1f05",
+    "url": "/static/media/loading.18dadafd.svg"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
---
+++ client/src/components/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import FontAwesome from 'react-fontawesome';
+
+// importting components
+
+// importing styles
+import { StyledFooter } from './StyledFooter';
+
+const Footer = () => (
+  <StyledFooter>
+    <div className="footer-content">
+      <a href="https://github.com/AditiDey" target="_blank">
+        Made with ❤ by Aditi
+      </a>
+      <a href="https://github.com/AditiDey/movierecommendation" target="_blank">
+        <FontAwesome className="fa fa-github" name="clock-o" />
+      </a>
+    </div>
+  </StyledFooter>
+);
+
+export default Footer;
